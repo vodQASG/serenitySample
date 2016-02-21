009ec23cf58427cd6469b607b0977731721d2d1c
Raman Kansal <ramankan@thoughtworks.com>
2016-02-21 11:00:47 +0800

    Raman - Added resourse folder

--- src/CucumberRunner.java
+++ src/CucumberRunner.java
@@ -5,7 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "/test/resources/features/*.feature")
+@CucumberOptions(features = "src/test/resources/features/*.feature")
 public class GoogleSearch {
 
 }
